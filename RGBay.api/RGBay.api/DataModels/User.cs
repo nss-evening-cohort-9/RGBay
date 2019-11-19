3b9264dcc0fc24bb8c7d3e71d343972adb632a55
Sean Davis <sean.dev.davis@gmail.com>
2019-11-18 20:47:50 -0600

    user dbo created, wip

---
+++ RGBay.api/RGBay.api/DataModels/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RGBay.api.DataModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ RGBay.api/RGBay.api/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using RGBay.api.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RGBay.api.Repositories
+{
+    public class UserRepository
+    {
+        string _connectionString = "Server=localhost;Database=RGBay;Trusted_Connection=True;";
+        public List<User> GetAll()
+        {
+            using (var db = new SqlConnection(_connectionString))
+            {
+                var users = db.Query<User>("Select * from User");
+                return users.ToList();
+            }
+        }
+    }
+}
