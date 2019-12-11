003f300ef71d752532bdfadd04c45464a8eb89e9
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-12-10 18:20:26 -0600

    readded config.sample.js file

---
+++ rgbay.ui/src/requests/config.sample.js
@@ -0,0 +1,12 @@
+// create a copy of this file and name it config.js, and add firebase api keys
+
+export default {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+};
