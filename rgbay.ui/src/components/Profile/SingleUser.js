05e923d93282518351e62755bcc3229b0e843920
Sean Davis <sean.dev.davis@gmail.com>
2019-12-10 19:35:29 -0600

    finished basic skeleton of UI with user info printing on profile page

--- rgbay.ui/src/components/Profile/SingleUser.js
+++ rgbay.ui/src/components/Profile/SingleUser.js
@@ -9,13 +9,16 @@ state = {
 
 getProfileInfo = (userId) => {
   userData
-  .getUserInfo(userId)
-  .then((user) => {
-      this.setState({ user: user })
-      console.log(user)
+  .getSingleUser(userId)
+  .then((res) => {
+      this.setState({ user: res.data })
       }
   )
   .catch(err => console.log("No information: ", err));
+}
+
+componentDidMount() {
+  this.getProfileInfo(this.props.match.params.id)
 }
 
 render() {
@@ -23,17 +26,14 @@ render() {
     return (
       <div className="container col-12 top-divide">
         <div className="container profile-top col-6">
-          {/* {/* <img src="https://i.pinimg.com/236x/c2/7c/27/c27c277903bc87b329a29005a1a371b2--funny-facebook-picture-collection.jpg" alt="profile-avi" className="bio-pic"></img> */}
+          <h3 className="textables">{user.username}</h3>
+          <img src="https://i.pinimg.com/236x/c2/7c/27/c27c277903bc87b329a29005a1a371b2--funny-facebook-picture-collection.jpg" alt="profile-avi" className="bio-pic"></img>
+          <div className="textables">{user.email}</div>
+          <div className="textables">{user.city}</div>
+          <div className="textables">{user.email}</div>
+          <div className="textables">{user.bio}</div>
         </div>
-
-        <div className="container profile-top col-6">
-          <div className="textables">Username: {user.id}</div>
-          <div className="textables">Email: {user.email}</div>
-          <div className="textables">City: {user.city}</div>
-          <div className="textables">Email: {user.email}</div>
-          <div className="textables">Bio:{user.bio}</div> */}
-        </div>
-          {/* <button className="btn btn-primary" onClick={this.getProfileInfo}>Test</button> */}
+          {/* <button className="btn btn-primary" onClick={this.getProfileInfo(this.props.match.params.id)}>Test</button> */}
       </div>
     )
   }
